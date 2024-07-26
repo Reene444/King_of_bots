543859f6df90c572b5ece6d0ff9697b7cd5a6486
Reene <67369326+Reene444@users.noreply.github.com>
2024-07-26 03:16:06 +0100

    config

--- RunningService/src/main/java/com/snakeio/snake/DemoApplication.java
+++ RunningService/src/main/java/com/snakeio/snake/DemoApplication.java
@@ -2,9 +2,9 @@ package com.snakeio.snake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
